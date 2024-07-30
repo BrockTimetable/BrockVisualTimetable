4806f9560d7409143feb17a8376f5717b73aad06
Oli-vers <180103373+Oli-vers@users.noreply.github.com>
2024-07-30 18:05:52 -0400

    Change navbar title

--- src/components/NavbarComponent.jsx
+++ src/components/NavbarComponent.jsx
@@ -18,7 +18,7 @@ export default function NavbarComponent() {
       <AppBar position="static">
         <Toolbar>
           <Typography variant="h5" component="div" sx={{ flexGrow: 1 }}>
-            Brock Timetable Generator
+            Brock Visual TimeTable
           </Typography>
           <IconButton sx={{ ml: 1 }} onClick={colorMode.toggleColorMode} color="inherit">
             {theme.palette.mode === 'dark' ? <Brightness7Icon /> : <Brightness4Icon />}
