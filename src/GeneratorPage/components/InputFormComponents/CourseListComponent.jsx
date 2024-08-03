a0dc37c163a62c3af69106ec3c839717b994d6c3
condyl <condyl678@gmail.com>
2024-08-03 15:43:09 -0400

    Change list subheader (remove spacing)

--- src/GeneratorPage/components/InputFormComponents/CourseListComponent.jsx
+++ src/GeneratorPage/components/InputFormComponents/CourseListComponent.jsx
@@ -1,4 +1,4 @@
-import React, { useContext} from "react";
+import React, { useContext } from "react";
 import CourseListItemComponent from "./CourseListItemComponent";
 import ListSubheader from "@mui/material/ListSubheader";
 import Box from "@mui/material/Box";
@@ -6,32 +6,32 @@ import List from "@mui/material/List";
 import { CourseDetailsContext } from "../../contexts/CourseDetailsContext";
 
 export default function CourseListComponent({ courses = [], onRemoveCourse }) {
-    const { courseDetails } = useContext(CourseDetailsContext);
-    return (
-        <Box sx={{ minWidth: 120 }} m={2}>
-            <List
-                sx={{ width: "100%", maxWidth: 360, bgcolor: "background.paper" }}
-                component="nav"
-                aria-labelledby="nested-list-subheader"
-                subheader={
-                    <ListSubheader component="div">
-                        Added Courses (Click to see more details):
-                    </ListSubheader>
-                }
-            >
-                {courses.map((course) => {
-                    const courseName = course.split(" ")[0] + course.split(" ")[1];
-                    const courseDetail = courseDetails.find(detail => detail.name === courseName);
-                    return (
-                        <CourseListItemComponent
-                            course={course}
-                            courseDetail={courseDetail}
-                            removeCourse={onRemoveCourse}
-                            key={course}
-                        />
-                    );
-                })}
-            </List>
-        </Box>
-    );
+  const { courseDetails } = useContext(CourseDetailsContext);
+  return (
+    <Box sx={{ minWidth: 120 }} m={2}>
+      <h5 style={{ fontWeight: "normal" }}>
+        Added Courses (Click to see more details):
+      </h5>
+      <List
+        sx={{ width: "100%", maxWidth: 360, bgcolor: "background.paper" }}
+        component="nav"
+        aria-labelledby="nested-list-subheader"
+      >
+        {courses.map((course) => {
+          const courseName = course.split(" ")[0] + course.split(" ")[1];
+          const courseDetail = courseDetails.find(
+            (detail) => detail.name === courseName
+          );
+          return (
+            <CourseListItemComponent
+              course={course}
+              courseDetail={courseDetail}
+              removeCourse={onRemoveCourse}
+              key={course}
+            />
+          );
+        })}
+      </List>
+    </Box>
+  );
 }
