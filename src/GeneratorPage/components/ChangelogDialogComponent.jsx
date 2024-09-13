57dcae46241c548b3f324c4a7b657eea4e7223c2
Oli-vers <180103373+Oli-vers@users.noreply.github.com>
2024-09-13 10:38:27 -0400

    Update change log dialog box

--- src/GeneratorPage/components/ChangelogDialogComponent.jsx
+++ src/GeneratorPage/components/ChangelogDialogComponent.jsx
@@ -8,12 +8,9 @@ import Button from "@mui/material/Button";
 const ChangelogDialogComponent = ({ open, handleClose }) => {
     return (
         <Dialog open={open} onClose={handleClose}>
-            <DialogTitle>Brock Visual Timetable - [Early Access Release]</DialogTitle>
+            <DialogTitle>Brock Visual Timetable Generator</DialogTitle>
             <DialogContent>
                 <div style={{ marginBottom: '20px' }}>
-                    <p style={{ color: 'red', fontSize: '0.775rem' }}>
-                        <strong>NOTE:</strong> This is an early access release. Features may not work as intended, and changes are expected in future updates. We appreciate your patience and feedback as we continue to improve the application.
-                    </p>
                     <h2>Tutorial Video</h2>
                     <p>
                         To ensure you have a smooth and efficient experience while creating your timetable,
@@ -28,7 +25,7 @@ const ChangelogDialogComponent = ({ open, handleClose }) => {
                 <div>
                     <p style={{ fontSize: '0.775rem' }}>
                         <strong>NOTE:</strong> The Brock Visual Timetable application was developed by an independent team of students with the goal of assisting other students and is not affiliated with, partnered, or endorsed
-                        by Brock University. The use of this application is at your own discretion. For any official information or updates related to Brock University, please refer directly to the
+                        by Brock University.<br></br><br></br> The use of this application is at your own discretion. For any official information or updates related to Brock University, please refer directly to the
                         university's official channels.<br></br><br></br>For any questions, concerns, feedback, or bug reports, please contact us at brocktimetable@gmail.com
                     </p>
                 </div>
