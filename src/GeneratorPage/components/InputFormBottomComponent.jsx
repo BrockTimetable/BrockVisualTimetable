84f2e59642e49cfe60e65c81a17662d6d28d4917
condyl <Condyl678@gmail.com>
2024-12-26 01:18:15 -0500

    Add tips section

--- src/GeneratorPage/components/InputFormBottomComponent.jsx
+++ src/GeneratorPage/components/InputFormBottomComponent.jsx
@@ -3,6 +3,7 @@ import Box from "@mui/material/Box";
 import Grid from "@mui/material/Grid";
 import CourseList from "./InputFormComponents/Sections/CourseList";
 import SourceCode from "./InputFormComponents/Sections/SourceCode";
+import Tips from "./InputFormComponents/Sections/Tips";
 import { removeCourseData } from "../scripts/courseData";
 import { removePinnedComponent } from "../scripts/pinnedComponents";
 
@@ -23,6 +24,9 @@ export default function InputFormBottomComponent({ addedCourses, setAddedCourses
             <Grid container spacing={2}>
                 <Grid item xs={12}>
                     <CourseList addedCourses={addedCourses} removeCourse={handleRemoveCourse} />
+                </Grid>
+                <Grid item xs={12}>
+                    <Tips />
                 </Grid>
                 <Grid item xs={12}>
                     <SourceCode />
