dba7d4a5b5d90817a919ebf82988a44ef604ef0e
Oli-vers <180103373+Oli-vers@users.noreply.github.com>
2025-02-06 15:41:23 -0500

    Update change log dialog for spring/summer courses

--- src/GeneratorPage/components/ChangelogDialogComponent.jsx
+++ src/GeneratorPage/components/ChangelogDialogComponent.jsx
@@ -8,11 +8,14 @@ import Button from "@mui/material/Button";
 const ChangelogDialogComponent = ({ open, handleClose }) => {
     return (
         <Dialog open={open} onClose={handleClose}>
-            <DialogTitle>Brock Visual Timetable Generator</DialogTitle>
             <DialogContent>
-                <div style={{ marginBottom: '20px' }}>
-                    <h2>Tutorial Video</h2>
-                    <p>
+                <div>
+                    <h2 style={{ fontSize: '1.1rem' }}>Spring/Summer 2025 Update</h2>
+                    <p style={{ fontSize: '0.85rem' }}>
+                        Spring & Summer 2025 course offerings are now available in the timetable generator!<br></br><br></br>Registration opens Wednesday, March 5th 2025 at 6:00AM EST on your my.brocku.ca portal.
+                    </p>
+                    <h2 style={{ fontSize: '1.1rem' }}>Tutorial Video</h2>
+                    <p style={{ fontSize: '0.85rem' }}>
                         To ensure you have a smooth and efficient experience while creating your timetable,
                         we highly recommend watching the tutorial video below.
                     </p>
@@ -23,10 +26,8 @@ const ChangelogDialogComponent = ({ open, handleClose }) => {
                 </div>
 
                 <div>
-                    <p style={{ fontSize: '0.775rem' }}>
-                        <strong>NOTE:</strong> The Brock Visual Timetable application was developed by an independent team of students with the goal of assisting other students and is not affiliated with, partnered, or endorsed
-                        by Brock University.<br></br><br></br> The use of this application is at your own discretion. For any official information or updates related to Brock University, please refer directly to the
-                        university's official channels.<br></br><br></br>For any questions, concerns, feedback, or bug reports, please contact us at brocktimetable@gmail.com
+                    <p style={{ fontSize: '0.6rem' }}>
+                        <strong>NOTE:</strong> The Brock Visual Timetable is not affiliated with, partnered, or endorsed by Brock University. The accuracy and availability of this application is not guaranteed.
                     </p>
                 </div>
             </DialogContent>
