f4de72f117e260844782b10c2a15a114602d9d05
condyl <Condyl678@gmail.com>
2026-01-03 12:51:37 -0500

    feat: add test configuration to Vite

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,12 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  test: {
+    include: [
+      "**/*.{test,spec}.?(c|m)[jt]s?(x)",
+      "**/__tests__/**/*.{test,spec}.?(c|m)[jt]s?(x)",
+      "**/*Test.?(c|m)[jt]s?(x)",
+      "**/__tests__/**/*Test.?(c|m)[jt]s?(x)",
+    ],
+  },
 });
